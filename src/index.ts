f39789e065f4d784be40607c42d119f7747da374
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2025-05-03 21:36:35 -0300

    criando o serviço de ETL do Github

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import { MarkdownService } from "./documentation/service/markdown/MarkdownService";
 export * from './model/models';
+import { GitHubService } from "./service/GitHubService";
 
 export class ReportManager {
 
+    public async githubETL(token:string,org: string, project: string){
+        const githubService = new GitHubService(token);
+        await githubService.ETLProject(org, project);
+        await githubService.ETLIssue(org, project);
+        await githubService.ETLBacklog(org, project);
+        await githubService.ETLTimeBox(org, project);
+
+    }
     public createReport(dbPath: string){
         const markdownService = new MarkdownService(dbPath);
         markdownService.createManagementDocumenation()
